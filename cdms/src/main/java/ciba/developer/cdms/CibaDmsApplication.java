fa96574e325987c1208c577d16c640acbd329919
Thembelani <46387159+Revelation301@users.noreply.github.com>
2021-02-01 10:42:19 +0200

    Initial commit with default project structure file tree

---
+++ cdms/src/main/java/ciba/developer/cdms/CibaDmsApplication.java
@@ -0,0 +1,13 @@
+package ciba.developer.cdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibaDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibaDmsApplication.class, args);
+	}
+
+}
---
+++ cdms/src/test/java/ciba/developer/cdms/CibaDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ciba.developer.cdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibaDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
